8bb71eb5d4fbc27801b13aa34f22f02d66b5d81a
Diego <marquez.diego.e@gmail.com>
2013-11-15 23:16:56 +0000

    Delete original mega example

--- examples/original/concrete_components/box_renderer.js
+++
@@ -1,9 +0,0 @@
-define(["renderer", "draw"], function(Renderer, Draw) {
-	var BoxRenderer = Renderer.extend({
-		draw: function(context) {
-			Draw.rectangle(context, -10, -10, 20, 20, this.color, this.color, 1);
-		}
-	});
-
-	return BoxRenderer;
-});
