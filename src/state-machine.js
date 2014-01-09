2f19e454af2cb2f88d01c0733492a676a5e65802
Diego <marquez.diego.e@gmail.com>
2014-01-09 00:56:12 +0000

    Started documenting state-machine.js

--- src/state-machine.js
+++ src/state-machine.js
@@ -1,3 +1,52 @@
+/**
+ * # state-machine.js
+ * ### By [Diego Enrique Marquez](http://www.treintipollo.com)
+ * ### [Find me on Github](https://github.com/diegomarquez)
+ *
+ * Inherits from: 
+ *
+ * Depends of: 
+ * [delegate](@@delegate@@)
+ * [class](@@class@@)
+ *
+ * A [requireJS](http://requirejs.org/) module. For use with [Game-Builder](http://diegomarquez.github.io/game-builder)
+ *
+ * This module allows you to setup [finite state machines](http://en.wikipedia.org/wiki/Finite-state_machine).
+ * It's a pretty useful pattern in game development, as it's a rather easy way to organize and coordinate different
+ * pieces of code without abusing _'if'_ statements.
+ * 
+ * This module has three main responsibilities:
+ *
+ * ### 1. Provide a factory for creating state machines and their states.
+ *
+ * The factory is what this module actually exposes, nothing special about it.
+ * 
+ * ### 2. Provides 2 types of state machines.
+ *
+ * The first **"loose"** type, allows you to just go from state to state, with no restriction. 
+ * It's pretty usefull, but states need to know about the other states in order to change control flow to them. 
+ * That isn't so good, but it works for small bits of code.
+ *
+ * The second **"fixed"** type, lets you configure the order of execution of states. In these state machines, a state needs know
+ * nothing about other states, which is a good thing. On it's insides, it just says it wants to go to the next state,
+ * and stuff happens. No need to know identifiers of any sort. It's a bit more rigid, but could save some headaches if used in the proper
+ * situation.
+ *
+ * ### 3. Provides a base class from which to extend your own states.
+ * 
+ * **States** have three main phases. **Initialization**, **Completion** and **Update**.
+ * The first two are functions executed when entering and exiting a state. **Update** must be called in a loop.
+ * All phases are optional. Ofcourse, having a state with no phases is a bit on the dumb side.
+ */
+
+/**
+ * Encapsulation is good
+ * --------------------------------
+ */
+
+/**
+ * --------------------------------
+ */
 define(["delegate", "class"], function(Delegate) {
 
 	var executeStateAction = function(stateId, action, args) {
@@ -29,6 +78,9 @@ define(["delegate", "class"], function(Delegate) {
 		return false;
 	}
 
+	/**
+	 * <p style='color:#AD071D'><strong>StateMachine</strong></p>
+	 */
 	var StateMachine = Class.extend({
 		init: function() {
 			this.stateIds = {}
@@ -69,7 +121,13 @@ define(["delegate", "class"], function(Delegate) {
 			this.states = null;
 		}
 	});
+	/**
+	 * --------------------------------
+	 */
 
+	/**
+	 * <p style='color:#AD071D'><strong>LooseStateMachine</strong> extends StateMachine</p>
+	 */
 	var LooseStateMachine = StateMachine.extend({
 		init: function() {
 			this._super();
@@ -88,7 +146,13 @@ define(["delegate", "class"], function(Delegate) {
 			this._super(state);
 		}		
 	});
+	/**
+	 * --------------------------------
+	 */
 
+	/**
+	 * <p style='color:#AD071D'><strong>FixedStateMachine</strong> extends StateMachine</p>
+	 */
 	var FixedStateMachine = StateMachine.extend({
 		init: function() {
 			this._super();
@@ -124,7 +188,13 @@ define(["delegate", "class"], function(Delegate) {
 			return this._super(state);
 		}		
 	});
-
+	/**
+	 * --------------------------------
+	 */
+	
+	/**
+	 * <p style='color:#AD071D'><strong>State</strong> extends [delegate](@@delegate@@)</p>
+	 */
 	var State = Delegate.extend({
 		init: function(scope, name) { 
 			this._super(); 
@@ -144,14 +214,22 @@ define(["delegate", "class"], function(Delegate) {
 		update: function(args) { this.execute('update', args); },
 		complete: function(args) { this.execute('complete', args); },
 	});
+	/**
+	 * --------------------------------
+	 */
 
-
+	/**
+	 * <p style='color:#AD071D'><strong>State Machine Factory</strong> extends [delegate](@@delegate@@)</p>
+	 */
 	var StateMachineFactory = {
 		createLooseStateMachine: function() { return new LooseStateMachine(); },
 		createFixedStateMachine: function() { return new FixedStateMachine(); },
 		
 		createState: function(scope, name) { return new State(scope, name); }
 	};
+	/**
+	 * --------------------------------
+	 */
 
 	return StateMachineFactory;
 });
