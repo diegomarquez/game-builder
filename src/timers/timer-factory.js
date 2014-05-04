377cff678d398592cb7d5e2db654d965bbd1b3d8
Diego <marquez.diego.e@gmail.com>
2014-05-04 19:55:09 +0100

    timer-factory.js now extends delegate.js

--- src/timers/timer-factory.js
+++ src/timers/timer-factory.js
@@ -4,6 +4,7 @@
  * ### [Find me on Github](https://github.com/diegomarquez)
  *
  * Inherits from:
+ * [delegate](@@delegate@@)
  *
  * Depends of: 
  * [timer](@@timer@@)
@@ -27,123 +28,281 @@ define(function(require) {
 
 	var ErrorPrinter = require('error-printer');
 
-	/**
-	 * ## **TimerFactory**
-	 */
-	var TimerFactory = function() {
-		this.timeOuts = [];
-	};
+	var TimerFactory = require('delegate').extend({
+		/**
+		 * <p style='color:#AD071D'><strong>init</strong></p>
+		 *
+		 * Constructor
+		 */
+		init: function() {
+			this._super();
+			this.timeOuts = [];
+		},
+		/**
+		 * --------------------------------
+		 */
+		
+		/**
+		 * <p style='color:#AD071D'><strong>get</strong></p>
+		 *
+		 * Creates a new instance of a Timer.
+		 * 
+		 * After creating it, it saves it into an array. It will also create a property in the **owner** 
+		 * giving it the name **propertyName**. 
+		 * 
+		 * @param  {Object} owner        Object in which the method was called in. Common value is _'this'_
+		 * @param  {String} name         An identifier for the Timer
+		 * @param  {String} propertyName Name of the property the timer will be added in **owner**.
+		 * @throws {Error} If the owner already has a property with the value of **propertyName**
+		 * @throws {Error} If owner is not passed as argument
+		 * @throws {Error} If name is not passed as argument
+		 * @throws {Error} If propertyName is not passed as a       
+		 */
+		get: function(owner, name, propertyName) {
+			if(owner.hasOwnProperty(propertyName)) {
+				ErrorPrinter.printError('Timer Factory', 'This owner is already using this property, assigning it again with a timer might cause your program to go ape shit.');
+			}else{
+				if (!owner) { 
+					ErrorPrinter.printError('Timer Factory', 'Timer must have an owner, if unsure just send in "this"');
+				}
 
-	/**
-	 * <p style='color:#AD071D'><strong>get</strong></p>
-	 *
-	 * Creates a new instance of a Timer.
-	 * 
-	 * After creating it, it saves it into an array. It will also create a property in the **owner** 
-	 * giving it the name **propertyName**. 
-	 * 
-	 * @param  {Object} owner        Object in which the method was called in. Common value is _'this'_
-	 * @param  {String} name         An identifier for the Timer
-	 * @param  {String} propertyName Name of the property the timer will be added in **owner**.
-	 * @throws {Error} If the owner already has a property with the value of **propertyName**
-	 * @throws {Error} If owner is not passed as argument
-	 * @throws {Error} If name is not passed as argument
-	 * @throws {Error} If propertyName is not passed as a       
-	 */
-	TimerFactory.prototype.get = function(owner, name, propertyName) {
-		if(owner.hasOwnProperty(propertyName)) {
-			ErrorPrinter.printError('Timer Factory', 'This owner is already using this property, assigning it again with a timer might cause your program to go ape shit.');
-		}else{
-			if (!owner) { 
-				ErrorPrinter.printError('Timer Factory', 'Timer must have an owner, if unsure just send in "this"');
+				if (!name) { 
+					ErrorPrinter.printError('Timer Factory', 'Timer must have an name to identify it later');
+				}
+				
+				if (!propertyName) { 
+					ErrorPrinter.printError('Timer Factory', 'Timer must have a propertyName to be refered with from its owners scope');
+				}
+
+				var timerConstructor = require('timer');
+
+				var timeout = new timerConstructor(owner, name, propertyName);
+				this.timeOuts.push(timeout);
+				owner[propertyName] = timeout;	
+
+				this.execute(this.CREATE, timeout);
+
+				var self = this;
+
+				// Remove the timer from the factory when it's REMOVE event is fired
+				timeout.on(timeout.REMOVE, function() {
+					index = self.timeOuts.indexOf(this);
+					self.timeOuts.splice(index, 1);
+
+					self.execute(self.REMOVE, timeout);
+				});
 			}
+		},
+		/**
+		 * --------------------------------
+		 */
+		
+		/**
+		 * <p style='color:#AD071D'><strong>getTotalCount</strong></p>
+		 *
+		 * @return {Number} The total amount of timers
+		 */
+		getTotalCount: function() {
+			return this.timeOuts.length;
+		},
+		/**
+		 * --------------------------------
+		 */
+		
+		/**
+		 * <p style='color:#AD071D'><strong>getRunningCount</strong></p>
+		 *
+		 * @return {Number} The amount of timers currently in a running state
+		 */
+		getRunningCount: function() {
+			return getCountInState.call(this, 'Running');
+		},
+		/**
+		 * --------------------------------
+		 */
+		
+		/**
+		 * <p style='color:#AD071D'><strong>getPausedCount</strong></p>
+		 *
+		 * @return {Number} The amount of timers currently in a paused state
+		 */
+		getPausedCount: function() {
+			return getCountInState.call(this, 'Paused');
+		},
+		/**
+		 * --------------------------------
+		 */
+		
+		/**
+		 * <p style='color:#AD071D'><strong>getStoppedCount</strong></p>
+		 *
+		 * @return {Number} The amount of timers currently in a stopped state
+		 */
+		getStoppedCount: function() {
+			return getCountInState.call(this, 'Stopped');
+		},
+		/**
+		 * --------------------------------
+		 */
 
-			if (!name) { 
-				ErrorPrinter.printError('Timer Factory', 'Timer must have an name to identify it later');
-			}
-			
-			if (!propertyName) { 
-				ErrorPrinter.printError('Timer Factory', 'Timer must have a propertyName to be refered with from its owners scope');
-			}
+		/*
+		These methods are pretty obvious in what they do. They are meant to apply a state to all registered timers
+		at the same time. Usefull to handle global state of an application.
 
-			var timerConstructor = require('timer');
+		For instance, if the application looses focus, you probably want to pause all timers, regardless of what they are doing
+		and to which part of the code base they belong to.
+		
+		After gaining focus, you can resume them all together. Pretty usefull.
 
-			var timeout = new timerConstructor(owner, name, propertyName);
-			this.timeOuts.push(timeout);
-			owner[propertyName] = timeout;	
-
-			var self = this;
-
-			// Remove the timer from the factory when it's REMOVE event is fired
-			timeout.on(timeout.REMOVE, function() {
-				index = self.timeOuts.indexOf(this);
-				self.timeOuts.splice(index, 1);
+		Although the names suggest obvious behaviour, the usage is not so obvious. Take a look at the object returned by **getChangeObject**
+		to see how to you can control timers in bulk.
+		 */
+		startAll: function() { 
+			return getChangeObject.call(this, 'start'); 
+		},
+		resetAll: function() { 
+			return getChangeObject.call(this, 'reset'); 
+		},
+		stopAll: function() { 
+			return getChangeObject.call(this, 'stop'); 
+		},
+		pauseAll: function() { 
+			return getChangeObject.call(this, 'pause'); 
+		},
+		resumeAll: function() { 
+			return getChangeObject.call(this, 'resume'); 
+		},
+		removeAll: function() { 
+			return getChangeObject.call(this, 'remove'); 
+		},
+		/**
+		 * --------------------------------
+		 */
+		
+		/**
+		 * <p style='color:#AD071D'><strong>setProperty</strong></p>
+		 *
+		 * This method is used to add or modify properties of all timers based
+		 * on a condition. If the condition evaluates to _'true'_ the property will be
+		 * set with the given value.
+		 * 
+		 * @param {Sring}    propertyName Name of the propety to set
+		 * @param {Anything} value        Value to set
+		 * @param {Function} condition    Condition that must be fullfilled before setting the property
+		 */
+		setProperty: function(propertyName, value, condition) {
+			this.timeOuts.forEach(function(timer, index, array) {
+				if(condition(timer, index, array)) {
+					timer[propertyName] = value;
+				}
 			});
-		}
-	};
+		},
+
+		/**
+		 * <p style='color:#AD071D'><strong>toMins</strong></p>
+		 *
+		 * @param  {Number} ms Milliseconds to convert to minutes
+		 *
+		 * @return {Number} The amount of minutes
+		 */
+		toMins: function(ms) { 
+			return parseInt(ms/(1000*60)); 
+		},
+		/**
+		 * --------------------------------
+		 */
+
+		/**
+		 * <p style='color:#AD071D'><strong>toSecs</strong></p>
+		 *
+		 * @param  {Number} ms Milliseconds to convert to seconds
+		 *
+		 * @return {Number} The amount of seconds
+		 */
+		toSecs: function(ms) { 
+			return parseInt((ms/1000));	
+		},
+		/**
+		 * --------------------------------
+		 */
+
+		/**
+		 * <p style='color:#AD071D'><strong>toMillisFraction</strong></p>
+		 *
+		 * @param  {Number} ms Milliseconds to truncate
+		 *
+		 * @return {Number} Truncated milliseconds 
+		 */
+		toMillisFraction: function(ms) { 
+			return parseInt((ms%1000)/100); 
+		},
+		/**
+		 * --------------------------------
+		 */
+		
+		/**
+		 * <p style='color:#AD071D'><strong>formatMinutes</strong></p>
+		 *
+		 * @param  {Number} ms Milliseconds to format
+		 *
+		 * @return {String} Formatted milliseconds to minutes
+		 */
+		formatMinutes: function(ms) { 
+			var mins = this.toMins(ms) % 60;
+	    	return (mins < 10) ? "0" + mins : mins;
+		},
+		/**
+		 * --------------------------------
+		 */
+
+		/**
+		 * <p style='color:#AD071D'><strong>formatSeconds</strong></p>
+		 *
+		 * @param  {Number} ms Milliseconds to format
+		 *
+		 * @return {String} Formatted milliseconds to seconds
+		 */
+		formatSeconds: function(ms) {
+			var fms = this.toMillisFraction(ms);
+			var secs = this.toSecs(ms) % 60;
+	    	return ((secs < 10) ? "0" + secs : secs) + '.' + fms;
+		},
+		/**
+		 * --------------------------------
+		 */
+
+		/**
+		 * <p style='color:#AD071D'><strong>formatMinutesSeconds</strong></p>
+		 *
+		 * @param  {Number} ms Milliseconds to format
+		 *
+		 * @return {String} Formatted milliseconds to minutes:seconds
+		 */
+		formatMinutesSeconds: function(ms) {
+			return this.formatMinutes(ms) + ':' + this.formatSeconds(ms);
+		},
+		/**
+		 * --------------------------------
+		 */
+	});
+
+	// ### Getters for all the types of events the sound player can hook into
+	Object.defineProperty(TimerFactory.prototype, "CREATE", { get: function() { return 'create'; } });
+	Object.defineProperty(TimerFactory.prototype, "REMOVE", { get: function() { return 'remove'; } });
 	/**
 	 * --------------------------------
 	 */
 
-	/*
-	These methods are pretty obvious in what they do. They are meant to apply a state to all registered timers
-	at the same time. Usefull to handle global state of an application.
+	var getCountInState = function(state) {
+		var result = 0;
 
-	For instance, if the application looses focus, you probably want to pause all timers, regardless of what they are doing
-	and to which part of the code base they belong to.
-	
-	After gaining focus, you can resume them all together. Pretty usefull.
-
-	Although the names suggest obvious behaviour, the usage is not so obvious. Keep reading to find out
-	how to use the control object these methods return.
-	 */
-	TimerFactory.prototype.startAll = function() { return getChangeObject.call(this, 'start'); }
-	TimerFactory.prototype.resetAll = function() { return getChangeObject.call(this, 'reset'); }
-	TimerFactory.prototype.stopAll = function() { return getChangeObject.call(this, 'stop'); }
-	TimerFactory.prototype.pauseAll = function() { return getChangeObject.call(this, 'pause'); }
-	TimerFactory.prototype.resumeAll = function() { return getChangeObject.call(this, 'resume'); }
-	TimerFactory.prototype.removeAll = function() { return getChangeObject.call(this, 'remove'); }	
-	/**
-	 * --------------------------------
-	 */
-	
-	/**
-	 * <p style='color:#AD071D'><strong>setProperty</strong></p>
-	 *
-	 * This method is used to add or modify properties of all timers based
-	 * on a condition. If the condition evaluates to _'true'_ the property will be
-	 * set with the given value.
-	 * 
-	 * @param {Sring}    propertyName Name of the propety to set
-	 * @param {Anything} value        Value to set
-	 * @param {Function} condition    Condition that must be fullfilled before setting the property
-	 */
-	TimerFactory.prototype.setProperty = function(propertyName, value, condition) {
-		this.timeOuts.forEach(function(timer, index, array) {
-			if(condition(timer, index, array)) {
-				timer[propertyName] = value;
+		for (var i = 0; i < this.timeOuts.length; i++) {
+			if(this.timeOuts[i][state]) {
+				result++;
 			}
-		});
-	}
+		}
 
-	// Some convenience methods to convert milliseconds to human friendly formats
-	TimerFactory.prototype.toMins = function(ms) { return parseInt(ms/(1000*60)); }
-	TimerFactory.prototype.toSecs = function(ms) { return parseInt((ms/1000));	}
-	TimerFactory.prototype.toMillisFraction = function(ms) { return parseInt((ms%1000)/100); }
-	
-	TimerFactory.prototype.formatMinutes = function(ms) { 
-		var mins = this.toMins(ms) % 60;
-    	return (mins < 10) ? "0" + mins : mins;
-	}
-
-	TimerFactory.prototype.formatSeconds = function(ms) {
-		var fms = this.toMillisFraction(ms);
-		var secs = this.toSecs(ms) % 60;
-    	return ((secs < 10) ? "0" + secs : secs) + '.' + fms;
-	}
-
-	TimerFactory.prototype.formatMinutesSeconds = function(ms) {
-		return this.formatMinutes(ms) + ':' + this.formatSeconds(ms);
+		return result;
 	}
 
 	var applyChangeToTimersIfTrue = function(state, condition) {
