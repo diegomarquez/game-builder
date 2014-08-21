38fc42c4aa76907b2d2b63ac7d135621773415d7
Diego <marquez.diego.e@gmail.com>
2014-08-21 01:06:38 +0100

    Add invert method to matrix-3x3.js

--- src/math/matrix-3x3.js
+++ src/math/matrix-3x3.js
@@ -172,6 +172,23 @@ define(function() {
 		return this;
 	};
 
+	p.invert = function() {
+		var a1 = this.a;
+		var b1 = this.b;
+		var c1 = this.c;
+		var d1 = this.d;
+		var tx1 = this.tx;
+		var n = a1*d1-b1*c1;
+
+		this.a = d1/n;
+		this.b = -b1/n;
+		this.c = -c1/n;
+		this.d = a1/n;
+		this.tx = (c1*this.ty-d1*tx1)/n;
+		this.ty = -(a1*this.ty-b1*tx1)/n;
+		return this;
+	};
+
 	p.identity = function() {
 		this.a = this.d = this.alpha = 1;
 		this.b = this.c = this.tx = this.ty = 0;
