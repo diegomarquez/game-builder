591c7dc796888dc357595ccda93c5e3b993fced4
Diego <marquez.diego.e@gmail.com>
2014-11-19 16:00:09 +0000

    Add default documentation to pre-load.js

--- src/pre-load.js
+++ src/pre-load.js
@@ -1,3 +1,18 @@
+/**
+ * # pre-load.js
+ * ### By [Diego Enrique Marquez](http://www.treintipollo.com)
+ * ### [Find me on Github](https://github.com/diegomarquez)
+ *
+ * Inherits from:
+ *
+ * Depends of:
+ * [font-loader](@@font-loader@@)
+ * 
+ * A [requireJS](http://requirejs.org/) module. For use with [Game-Builder](http://diegomarquez.github.io/game-builder)
+ * 
+ * This module bootstraps the main module. It makes sure fonts are loaded before initializing the main application
+ */
+
 define(['require', 'domready!', 'font-loader'], function(require, dom, fontLoader) {
     // Start loading configured fonts
     fontLoader.start(function() {
