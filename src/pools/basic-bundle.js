26bb57f4e055b56dab368ff33cbe6c7848b809b6
Diego <marquez.diego.e@gmail.com>
2015-01-12 13:19:52 +0000

    Enhance pool bundles

---
+++ src/pools/basic-bundle.js
@@ -0,0 +1,70 @@
+/**
+ * # basic-bundle.js
+ * ### By [Diego Enrique Marquez](http://www.treintipollo.com)
+ * ### [Find me on Github](https://github.com/diegomarquez)
+ *
+ * Inherits from:
+ * [bundle](@@bundle@@)
+ *
+ * Depends of: 
+ * [circle-collider](@@circle-collider@@)
+ * [polygon-collider](@@polygon-collider@@)
+ * [fixed-polygon-collider](@@fixed-polygon-collider@@)
+ * [path-renderer](@@path-renderer@@)
+ * [bitmap-renderer](@@bitmap-renderer@@)
+ * [text-renderer](@@text-renderer@@)
+ * [game-object](@@game-object@@)
+ * [game-object-container](@@game-object-container@@)
+ *
+ * A [requireJS](http://requirejs.org/) module. For use with [Game-Builder](http://diegomarquez.github.io/game-builder)
+ * 
+ * This bundle configures the [game-object-pool](@@game-object-pool@@) and the [component-pool](@@component-pool@@) so that the
+ * basic [components](@@component@@) and [game-objects](@@game-object@@) are ready to use.
+ *
+ * If you need a configuration to use a simple [game-object](@@game-object@@) or a [game-object-container](@@game-object-container@@)
+ * there is no need to setup a pool for them, instead you can just use the **getGameObjectPoolId** and **getGameObjectContainerPoolId**
+ * methods where necessary. 
+ *
+ * Same goes for [renderers](@@renderer@@) if you need ids for a [bitmap-renderer](@@bitmap-renderer@@), 
+ * [text-renderer](@@text-renderer@@) or [path-renderer](@@path-renderer@@) you can use the 
+ * **getBitmapRendererPoolId**, **getTextRendererPoolId** and **getPathRendererPoolId** methods respectively.
+ *
+ * Similarly you can get ids for [collision-components](@@collision-component@@), either a [circle-collider](@@circle-collider@@), 
+ * [polygon-collider](@@polygon-collider@@) or [fixed-polygon-collider](@@fixed-polygon-collider@@) have their corresponding methods. 
+ * **getCircleColliderPoolId**, **getPolygonColliderPoolId** and **getFixedPolygonColliderPoolId**.
+ * 
+ */
+
+/**
+ * --------------------------------
+ */
+define(function(require) {
+	var BasicBundle = require('bundle').extend({
+	
+		/**
+		 * <p style='color:#AD071D'><strong>create</strong></p>
+		 *
+		 * This method configures the [game-object-pool](@@game-object-pool@@) and the [component-pool](@@component-pool@@)
+		 * with the basic objects so it is not needed to define them repeatedly on other bundles
+		 * 
+		 * @param  {Object} [args=null] 
+		 */
+		create: function(args) {
+			this.createComponentPool(function() { return require('circle-collider') });
+			this.createComponentPool(function() { return require('polygon-collider') });
+			this.createComponentPool(function() { return require('fixed-polygon-collider') });
+
+			this.createComponentPool(function() { return require('path-renderer') });
+			this.createComponentPool(function() { return require('bitmap-renderer') });
+			this.createComponentPool(function() { return require('text-renderer') });
+
+			this.createDynamicGameObjectPool(function() { return require('game-object') });
+			this.createDynamicGameObjectPool(function() { return require('game-object-container') });
+		},
+		/**
+		 * --------------------------------
+		 */
+	});
+
+	return BasicBundle;
+});
--- src/pools/bundle.js
+++ src/pools/bundle.js
@@ -15,12 +15,58 @@
  * This is a helper module to define a block of [game-object-pool](@@game-object-pool@@) and 
  * [component-pool](@@component-pool@@) configurations. 
  *
- * The basic workflow is extending this module, and putting all the required code
+ * The basic workflow is extending this module and putting all the required code
  * to configure the pools in there. That way you can reuse that code in different places.
  *
- * It's not really needed but it will make things a bit more tidy.
- * Because it has [gb](@@gb@@) as a dependency it already has references to the pools
- * and the canvas, so you don't have to require those in the modules extending this one.
+ * The module defines five convinience methods 
+ * **createComponentPool**, 
+ * **createGameObjectPool**, 
+ * **createDynamicGameObjectPool**, 
+ * **createComponentConfiguration** and
+ * **createGameObjectConfiguration**
+ *
+ * These should make setting up the [game-object-pool](@@game-object-pool@@) and the [component-pool](@@component-pool@@) a little easier.
+ *
+ * An additional feature is that getter methods are created dynamically in the bundle prototype for pools and configurations 
+ * defined using any of the methods described above. 
+ * These dynamic methods return the ids used for a pool or a configuration. In the case that you want to use a configuration id 
+ * or a pool id defined in a bundle, you don't have to use the raw string in your code, instead you can **require** the corresponding 
+ * bundle, and call one of these methods to get the id you are looking for.
+ *
+ * Ej.
+ * ```
+ * // This method sets up a component pool using the 'circle-collider' module
+ * this.createComponentPool(function() { return require('circle-collider') });
+ *
+ * // As a result this method is created in the prototype 
+ * // which returns the string 'circle-collider'
+ * this.getCircleColliderPoolId()
+ *
+ * ```
+ *
+ * Ej.
+ * ```
+ *
+ * // This method sets up a game object pool using the 'game-object' module
+ * this.createGameObjectPool(function() { return require('game-object') }, 10);
+ *
+ * // As a result this method is created in the prototype 
+ * // which returns the string 'game-object'
+ * this.getGameObjectPoolId()
+ * 
+ * ```
+ *
+ * Ej.
+ * ```
+ *
+ * // This method creates a configuration for a game object pool
+ * this.createGameObjectConfiguration('Object_1', 'Pool_1');
+ *
+ * // As a result this method is created in the prototype 
+ * // which returns the string 'Object_1'
+ * this.getObject_1ConfigurationId()
+ * 
+ * ```
  */
 
 /**
@@ -38,10 +84,10 @@ define(function(require) {
 			
 			this.gameObjectPool = gb.goPool;
 			this.componentPool 	= gb.coPool;
-			this.canvas 		= gb.canvas;
+			this.canvas 				= gb.canvas;
 			this.assetMap       = gb.assetMap();
 		},
-
+		
 		/**
 		 * <p style='color:#AD071D'><strong>create</strong></p>
 		 *
@@ -56,7 +102,137 @@ define(function(require) {
 		/**
 		 * --------------------------------
 		 */
+		
+		/**
+		 * <p style='color:#AD071D'><strong>createComponentPool</strong></p>
+		 *
+		 * Creates a component pool. The name is generated from the function provided as argument
+		 *
+		 * ```
+		 * this.createComponentPool(function() { return require('a-component-module-name')});
+		 * ```
+		 *
+		 * It is necessary to use that syntax so the name can be infered from the argument
+		 * 
+		 * @param  {Function} moduleGetter A function that returns the result of a **require** call
+		 */
+		createComponentPool = function (moduleGetter) {
+			var name = getModuleName(moduleGetter);
+
+			this.componentPool.createPool(name, moduleGetter());
+		},
+		/**
+		 * --------------------------------
+		 */
+
+		/**
+		 * <p style='color:#AD071D'><strong>createGameObjectPool</strong></p>
+		 *
+		 * Creates a game object pool. The name is generated from the function provided as argument
+		 *
+		 * ```
+		 * this.createGameObjectPool(function() { return require('a-game-object-module-name')}, 10);
+		 * ```
+		 *
+		 * It is necessary to use that syntax so the name can be infered from the argument
+		 * 
+		 * @param  {Function} moduleGetter A function that returns the result of a **require** call
+		 * @param  {Number} amount       The amount of object the pool will hold
+		 */
+		createGameObjectPool = function (moduleGetter, amount) {
+			var name = getModuleName(moduleGetter);
+
+			this.gameObjectPool.createPool(name, moduleGetter(), amount);
+
+			addPoolIdGetter.call(this, name);
+		},
+		/**
+		 * --------------------------------
+		 */
+
+		/**
+		 * <p style='color:#AD071D'><strong>createDynamicGameObjectPool</strong></p>
+		 *
+		 * Creates a a game object pool. The name is generated from the function provided as argument
+		 *
+		 * ```
+		 * this.createDynamicGameObjectPool(function() { return require('a-game-object-module-name')});
+		 * ```
+		 *
+		 * It is necessary to use that syntax so the name can be infered from the argument
+		 * 
+		 * @param  {Function} moduleGetter A function that returns the result of a **require** call
+		 */
+		createDynamicGameObjectPool = function (moduleGetter) {
+			var name = getModuleName(moduleGetter);
+
+			this.gameObjectPool.createDynamicPool(name, moduleGetter());
+
+			addPoolIdGetter.call(this, name);
+		},
+		/**
+		 * --------------------------------
+		 */
+
+		/**
+		 * <p style='color:#AD071D'><strong>createComponentConfiguration</strong></p>
+		 *
+		 * @param  {[type]} configurationId [description]
+		 * @param  {[type]} poolId          [description]
+		 *
+		 * @return {[type]}                 [description]
+		 */
+		createComponentConfiguration = function (configurationId, poolId) {
+			var configuration = this.componentPool.createConfiguration(configurationId, poolId);
+
+			addConfigurationIdGetter.call(this, configurationId);
+
+			return configuration;
+		},
+		/**
+		 * --------------------------------
+		 */
+
+		/**
+		 * <p style='color:#AD071D'><strong>createGameObjectConfiguration</strong></p>
+		 *
+		 * @param  {[type]} configurationId [description]
+		 * @param  {[type]} poolId          [description]
+		 *
+		 * @return {[type]}                 [description]
+		 */
+		createGameObjectConfiguration = function (configurationId, poolId) {
+			var configuration = this.gameObjectPool.createConfiguration(configurationId, poolId);
+			
+			addConfigurationIdGetter.call(this, configurationId);
+
+			return configuration;
+		}
+		/**
+		 * --------------------------------
+		 */
 	});
+
+	var getModuleName = function(moduleGetter) {
+		return moduleGetter.toString().match(/require\(['|"](.*?)['|"]\)/)[1]
+	}
+
+	var addPoolIdGetter = function(name) {
+		var n = name.split('-').map(function (word) { return word.charAt(0).toUpperCase() + word.slice(1); }).join("");
+		var method = 'get' + n + 'PoolId';
+		
+		this.constructor.prototype[method] = function() { 
+			return name; 
+		}
+	}
+
+	var addConfigurationIdGetter = function(name) {
+		var method = 'get' + name + 'ConfigurationId';
+		
+		this.constructor.prototype[method] = function() { 
+			return name; 
+		}
+	}
 
 	return Bundle;
 });
