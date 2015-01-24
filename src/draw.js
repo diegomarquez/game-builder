8299f881d4ac0b3cb0655c72def14d741d74eaa2
Diego <marquez.diego.e@gmail.com>
2015-01-24 16:36:57 +0000

    Update draw.js with several things to have better control over paths

--- src/draw.js
+++ src/draw.js
@@ -44,16 +44,20 @@ define(function() {
 	 * @param  {String|Number} [fillColor=null the circle can be a [hex string](http://www.javascripter.net/faq/hextorgb.htm) or a number (If you are into that kind of bullshit)
 	 * @param  {String|Number} [strokeColor=null the circle can be a [hex string](http://www.javascripter.net/faq/hextorgb.htm) or a number
 	 * @param  {Number} [lineWidth=null] Line width             	
+	 * @param  {Boolean} [close=true] Whether the path should be closed or not
 	 */
-	DrawUtils.prototype.circle = function(context, x, y, radius, fillColor, strokeColor, lineWidth) {
+	DrawUtils.prototype.circle = function(context, x, y, radius, fillColor, strokeColor, lineWidth, close) {
 		if (fillColor) context.fillStyle = fillColor;
 		if (strokeColor) context.strokeStyle = strokeColor;
 		if (lineWidth) context.lineWidth = lineWidth;
 
+		if (close === undefined) close = true;
+
 		context.beginPath();
 		context.arc(x, y, radius, 0, Math.PI * 2, false);
-		context.closePath();
 
+		if (close) context.closePath();
+		
 		if (fillColor) context.fill();
 		if (strokeColor) context.stroke();
 	}
@@ -73,16 +77,20 @@ define(function() {
 	  * @param  {Number} height      Height of the rectangle
 	  * @param  {String|Number} [fillColor=null]   Fill color for the rectangle can be a [hex string](http://www.javascripter.net/faq/hextorgb.htm) or a number (If you are into that kind of bullshit)
 	  * @param  {String|Number} [strokeColor=null] Stroke color for the rectangle can be a [hex string](http://www.javascripter.net/faq/hextorgb.htm) or a number (If you are into that kind of bullshit)
-	  * @param  {Number} [lineWidth=null]   Line width            
+	  * @param  {Number} [lineWidth=null]   Line width
+	  * @param  {Boolean} [close=true] Whether the path should be closed or not            
 	  */
-	DrawUtils.prototype.rectangle = function(context, x, y, width, height, fillColor, strokeColor, lineWidth) {
+	DrawUtils.prototype.rectangle = function(context, x, y, width, height, fillColor, strokeColor, lineWidth, close) {
 		if (strokeColor) context.strokeStyle = strokeColor;
 		if (lineWidth) context.lineWidth = lineWidth;
 		if (fillColor) context.fillStyle = fillColor;
 
+		if (close === undefined) close = true;
+
 		context.beginPath();
 		context.rect(x, y, width, height);
-		context.closePath();
+		
+		if (close) context.closePath();
 
 		if (fillColor) context.fill();
 		if (strokeColor) context.stroke();
@@ -109,11 +117,14 @@ define(function() {
 	  * @param  {String|Number} [strokeColor=null] Stroke color for the triangle can be a [hex string](http://www.javascripter.net/faq/hextorgb.htm) or a number (If you are into that kind of bullshit)
 	  * @param  {Number} [lineWidth=null]   Line width
 	  * @param  {Number} [scale=1] Scale of the triangle. This can be usefull to avoid recalculating vertex position             
+	  * @param  {Boolean} [close=true] Whether the path should be closed or not
 	  */
-	DrawUtils.prototype.triangle = function(context, centerX, centerY, x1, y1, x2, y2, x3, y3, fillColor, strokeColor, lineWidth, scale) {
+	DrawUtils.prototype.triangle = function(context, centerX, centerY, x1, y1, x2, y2, x3, y3, fillColor, strokeColor, lineWidth, scale, close) {
 		if (strokeColor) context.strokeStyle = strokeColor;
 		if (lineWidth) context.lineWidth = lineWidth;
 		if (fillColor) context.fillStyle = fillColor;
+
+		if (close === undefined) close = true;
 
 		if (!scale) scale = 1;
 
@@ -137,7 +148,7 @@ define(function() {
 		context.lineTo(x2, y2);
 		context.lineTo(x3, y3);
 
-		context.closePath();
+		if (close) context.closePath();
 
 		if (fillColor) context.fill();
 		if (strokeColor) context.stroke();
@@ -171,11 +182,14 @@ define(function() {
 	  * @param  {String|Number} [strokeColor=null] Stroke color for the triangle can be a [hex string](http://www.javascripter.net/faq/hextorgb.htm) or a number (If you are into that kind of bullshit)
 	  * @param  {Number} [lineWidth=null]   Line width
 	  * @param  {Number} [scale=1] Scale of the triangle. This can be usefull to avoid recalculating vertex position
+	  * @param  {Boolean} [close=true] Whether the path should be closed or not
 	  */
-	DrawUtils.prototype.quadraticTriangle = function(context, centerX, centerY, x1, y1, ax1, ay1, x2, y2, ax2, ay2, x3, y3, ax3, ay3, fillColor, strokeColor, lineWidth, scale) {
+	DrawUtils.prototype.quadraticTriangle = function(context, centerX, centerY, x1, y1, ax1, ay1, x2, y2, ax2, ay2, x3, y3, ax3, ay3, fillColor, strokeColor, lineWidth, scale, close) {
 		if (strokeColor) context.strokeStyle = strokeColor;
 		if (lineWidth) context.lineWidth = lineWidth;
 		if (fillColor) context.fillStyle = fillColor;
+
+		if (close === undefined) close = true;
 
 		if (!scale) scale = 1;
 
@@ -212,7 +226,7 @@ define(function() {
 		context.quadraticCurveTo(ax2, ay2, x3, y3);
 		context.quadraticCurveTo(ax3, ay3, x1, y1);
 
-		context.closePath();
+		if (close) context.closePath();
 
 		if (fillColor) context.fill();
 		if (strokeColor) context.stroke();
@@ -234,11 +248,14 @@ define(function() {
 	  * @param  {String|Number} [strokeColor=null] Stroke color for the polygon can be a [hex string](http://www.javascripter.net/faq/hextorgb.htm) or a number (If you are into that kind of bullshit)
 	  * @param  {Number} [lineWidth=null]   Line width
 	  * @param  {Number} [scale=1] Scale of the polygon. This can be usefull to avoid recalculating vertex position
+	  * @param  {Boolean} [close=true] Whether the path should be closed or not
 	  */
-	DrawUtils.prototype.polygon = function(context, x, y, points, fillColor, strokeColor, lineWidth, scale) {
+	DrawUtils.prototype.polygon = function(context, x, y, points, fillColor, strokeColor, lineWidth, scale, close) {
 		if (strokeColor) context.strokeStyle = strokeColor;
 		if (lineWidth) context.lineWidth = lineWidth;
 		if (fillColor) context.fillStyle = fillColor;
+
+		if (close === undefined) close = true;
 
 		if (!scale) scale = 1;
 
@@ -250,7 +267,55 @@ define(function() {
 			context.lineTo((points[i].x * scale) + x, (points[i].y * scale) + y);
 		}
 
-		context.closePath();
+		if (close) context.closePath();
+
+		if (fillColor) context.fill();
+		if (strokeColor) context.stroke();
+	}
+	/**
+	 * --------------------------------
+	 */
+	
+	/**
+	  * <p style='color:#AD071D'><strong>relativePolygon</strong> </p>
+	  *
+	  * This one is pretty usefull. Send in an array of points and lines will be ploted clock-wise to form the polygon.
+	  * Each point is relative to the last one. 
+	  * 
+	  * @param  {Context 2D} context [CanvasRenderingContext2D](https://developer.mozilla.org/en-US/docs/Web/API/CanvasRenderingContext2D)
+	  * @param  {Number} x           X coordinate registration point. All the points of the polygon are relative to this
+	  * @param  {Number} y           Y coordinate registration point. All the points of the polygon are relative to this
+	  * @param  {Array} points       An array of objects with the following form, {x:x, y:y}
+	  * @param  {String|Number} [fillColor=null]   Fill color for the polygon can be a [hex string](http://www.javascripter.net/faq/hextorgb.htm) or a number (If you are into that kind of bullshit)
+	  * @param  {String|Number} [strokeColor=null] Stroke color for the polygon can be a [hex string](http://www.javascripter.net/faq/hextorgb.htm) or a number (If you are into that kind of bullshit)
+	  * @param  {Number} [lineWidth=null]   Line width
+	  * @param  {Number} [scale=1] Scale of the polygon. This can be usefull to avoid recalculating vertex position
+	  * @param  {Boolean} [close=true] Whether the path should be closed or not
+	  */
+	DrawUtils.prototype.relativePolygon = function(context, x, y, points, fillColor, strokeColor, lineWidth, scale, close) {
+		if (strokeColor) context.strokeStyle = strokeColor;
+		if (lineWidth) context.lineWidth = lineWidth;
+		if (fillColor) context.fillStyle = fillColor;
+
+		if (close === undefined) close = true;
+
+		if (!scale) scale = 1;
+
+		context.beginPath();
+
+		context.moveTo((points[0].x * scale) + x, (points[0].y * scale) + y);
+		
+		var lastAbsoluteX = points[0].x;
+		var lastAbsoluteY = points[0].y;
+
+		for (var i = 1; i < points.length; i++) {
+			context.lineTo( ((points[i].x + lastAbsoluteX) * scale) + x, ((points[i].y + lastAbsoluteY)  * scale) + y );
+
+			lastAbsoluteX = points[i].x + lastAbsoluteX;
+			lastAbsoluteY = points[i].y + lastAbsoluteY;
+		}
+
+		if (close) context.closePath();
 
 		if (fillColor) context.fill();
 		if (strokeColor) context.stroke();
@@ -272,12 +337,15 @@ define(function() {
 	  * @param  {String|Number} [strokeColor=null] Stroke color for the polygon can be a [hex string](http://www.javascripter.net/faq/hextorgb.htm) or a number (If you are into that kind of bullshit)
 	  * @param  {Number} [lineWidth=null]   Line width
 	  * @param  {Number} [scale=1] Scale of the polygon. This can be usefull to avoid recalculating vertex position
+	  * @param  {Boolean} [close=true] Whether the path should be closed or not
 	  */
-	DrawUtils.prototype.quadraticPolygon = function(context, x, y, points, fillColor, strokeColor, lineWidth, scale) {
+	DrawUtils.prototype.quadraticPolygon = function(context, x, y, points, fillColor, strokeColor, lineWidth, scale, close) {
 		if (strokeColor) context.strokeStyle = strokeColor;
 		if (lineWidth) context.lineWidth = lineWidth;
 		if (fillColor) context.fillStyle = fillColor;
 
+		if (close === undefined) close = true;
+		
 		if (!scale) scale = 1;
 
 		context.beginPath();
@@ -295,8 +363,147 @@ define(function() {
 			context.quadraticCurveTo((points[i].x * scale) + x, (points[i].y * scale) + y, (points[next].x * scale) + x, (points[next].y * scale) + y);
 		}
 
-		context.closePath();
+		if (close) context.closePath();
 
+		if (fillColor) context.fill();
+		if (strokeColor) context.stroke();
+	}
+	/**
+	 * --------------------------------
+	 */
+	
+	/**
+	  * <p style='color:#AD071D'><strong>quadraticPolygonAuto</strong></p>
+	  *
+	  * This is like the polygon but the lines between vertexes are drawn as quadratic curves.
+	  * The anchor points for the quadratic curves are generated, by calculating the normals between each pair of points.
+	  * The anchor points are generated alternating sides.
+	  * 
+	  * @param  {Context 2D} context [CanvasRenderingContext2D](https://developer.mozilla.org/en-US/docs/Web/API/CanvasRenderingContext2D)
+	  * @param  {Number} x           X coordinate registration point. All the points of the polygon are relative to this
+	  * @param  {Number} y           Y coordinate registration point. All the points of the polygon are relative to this
+	  * @param  {Number} anchorDistance Distance to which generated anchor points are placed from thir respective segments
+	  * @param  {String} points      An array of objects with the following form, {x:x, y:y}.
+	  * @param  {String|Number} [fillColor=null]   Fill color for the polygon can be a [hex string](http://www.javascripter.net/faq/hextorgb.htm) or a number (If you are into that kind of bullshit)
+	  * @param  {String|Number} [strokeColor=null] Stroke color for the polygon can be a [hex string](http://www.javascripter.net/faq/hextorgb.htm) or a number (If you are into that kind of bullshit)
+	  * @param  {Number} [lineWidth=null]   Line width
+	  * @param  {Number} [scale=1] Scale of the polygon. This can be usefull to avoid recalculating vertex position
+	  * @param  {Boolean} [positive=false] On which side the anchor points should start alternation
+	  * @param  {Boolean} [close=true] Whether the path should be closed or not
+	  */
+	DrawUtils.prototype.quadraticPolygonAuto = function(context, x, y, anchorDistance, points, fillColor, strokeColor, lineWidth, scale, positive, close) {
+		if (strokeColor) context.strokeStyle = strokeColor;
+		if (lineWidth) context.lineWidth = lineWidth;
+		if (fillColor) context.fillStyle = fillColor;
+
+		if (close === undefined) close = true;
+
+		if (!scale) scale = 1;
+
+		context.beginPath();
+
+		var diffX, diffY;
+		var anchorX, anchorY;
+		var normalSide = positive;
+
+		for (var i = 1; i < points.length; i++) {
+			diffX = (points[i].x + points[i-1].x)/2; 
+			diffY = (points[i].y + points[i-1].y)/2;
+
+			var angle = Math.atan2(diffY, diffX) + Math.PI/2;
+
+			if (normalSide) {
+				anchorX = diffX + Math.cos(angle) * anchorDistance;
+				anchorY = diffY + Math.sin(angle) * anchorDistance;
+			} else {
+				anchorX = diffX + Math.cos(angle) * -anchorDistance;
+				anchorY = diffY + Math.sin(angle) * -anchorDistance;
+			}
+
+			context.moveTo((points[i-1].x * scale) + x, (points[i-1].y * scale) + y);
+			context.quadraticCurveTo((anchorX * scale) + x, (anchorY * scale) + y, (points[i].x * scale) + x, (points[i].y * scale) + y);
+
+			normalSide = !normalSide;
+		}
+
+		if (close) {
+			context.moveTo((points[points.length-1].x * scale) + x, (points[points.length-1].y * scale) + y);
+			context.lineTo((points[0].x * scale) + x, (points[0].y * scale) + y);	
+		}
+		
+		if (fillColor) context.fill();
+		if (strokeColor) context.stroke();
+	}
+	/**
+	 * --------------------------------
+	 */
+	
+	/**
+	  * <p style='color:#AD071D'><strong>realtiveQuadraticPolygonAuto</strong></p>
+	  *
+	  * This is like the polygon but the lines between vertexes are drawn as quadratic curves.
+	  * The anchor points for the quadratic curves are generated, by calculating the normals between each pair of points.
+	  * The anchor points are generated alternating sides.
+	  * Each point is relative to the last one.
+	  * 
+	  * @param  {Context 2D} context [CanvasRenderingContext2D](https://developer.mozilla.org/en-US/docs/Web/API/CanvasRenderingContext2D)
+	  * @param  {Number} x           X coordinate registration point. All the points of the polygon are relative to this
+	  * @param  {Number} y           Y coordinate registration point. All the points of the polygon are relative to this
+	  * @param  {Number} anchorDistance Distance to which generated anchor points are placed from thir respective segments
+	  * @param  {String} points      An array of objects with the following form, {x:x, y:y}.
+	  * @param  {String|Number} [fillColor=null]   Fill color for the polygon can be a [hex string](http://www.javascripter.net/faq/hextorgb.htm) or a number (If you are into that kind of bullshit)
+	  * @param  {String|Number} [strokeColor=null] Stroke color for the polygon can be a [hex string](http://www.javascripter.net/faq/hextorgb.htm) or a number (If you are into that kind of bullshit)
+	  * @param  {Number} [lineWidth=null]   Line width
+	  * @param  {Number} [scale=1] Scale of the polygon. This can be usefull to avoid recalculating vertex position
+	  * @param  {Boolean} [positive=false] On which side the anchor points should start alternation
+	  * @param  {Boolean} [close=true] Whether the path should be closed or not
+	  */
+	DrawUtils.prototype.realtiveQuadraticPolygonAuto = function(context, x, y, anchorDistance, points, fillColor, strokeColor, lineWidth, scale, positive, close) {
+		if (strokeColor) context.strokeStyle = strokeColor;
+		if (lineWidth) context.lineWidth = lineWidth;
+		if (fillColor) context.fillStyle = fillColor;
+
+		if (close === undefined) close = true;
+
+		if (!scale) scale = 1;
+
+		context.beginPath();
+
+		var diffX, diffY;
+		var anchorX, anchorY;
+		var normalSide = positive;
+
+		var lastAbsoluteX = points[0].x;
+		var lastAbsoluteY = points[0].y;
+
+		for (var i = 1; i < points.length; i++) {
+			diffX = (points[i].x + points[i-1].x)/2; 
+			diffY = (points[i].y + points[i-1].y)/2;
+
+			var angle = Math.atan2(diffY, diffX) + Math.PI/2;
+
+			if (normalSide) {
+				anchorX = diffX + Math.cos(angle) * anchorDistance;
+				anchorY = diffY + Math.sin(angle) * anchorDistance;
+			} else {
+				anchorX = diffX + Math.cos(angle) * -anchorDistance;
+				anchorY = diffY + Math.sin(angle) * -anchorDistance;
+			}
+
+			context.moveTo((points[i-1].x * scale) + x, (points[i-1].y * scale) + y);
+			context.quadraticCurveTo((anchorX * scale) + x, (anchorY * scale) + y, ((points[i].x + lastAbsoluteX) * scale) + x, ((points[i].y + lastAbsoluteY) * scale) + y);
+
+			normalSide = !normalSide;
+
+			lastAbsoluteX = points[i].x + lastAbsoluteX;
+			lastAbsoluteY = points[i].y + lastAbsoluteY;
+		}
+
+		if (close) {
+			context.moveTo((points[points.length-1].x * scale) + x, (points[points.length-1].y * scale) + y);
+			context.lineTo((points[0].x * scale) + x, (points[0].y * scale) + y);	
+		}
+		
 		if (fillColor) context.fill();
 		if (strokeColor) context.stroke();
 	}
